5322c913fd4812bbf6a75a9e307da2baaf0625cd
Romberto <korhoff@mail.ru>
2024-04-25 18:50:00 +0300

    6

--- django-backend/src/proj/settings.py
+++ django-backend/src/proj/settings.py
@@ -45,7 +45,7 @@ SECRET_KEY = SK
 DEBUG = True
 
 if DEBUG:
-    ALLOWED_HOSTS = ["localhost", '127.0.0.1', '0.0.0.0']
+    ALLOWED_HOSTS = ["localhost", '127.0.0.1', '0.0.0.0', 'promoto.tw1.ru']
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -58,7 +58,7 @@ if DEBUG:
     }
 
 else:
-    ALLOWED_HOSTS = ['localhost', '188.225.72.113', '1566825-cw73062.twc1.net', '127.0.0.1']
+    ALLOWED_HOSTS = ['localhost', '188.225.72.113', '1566825-cw73062.twc1.net', '127.0.0.1', 'promoto.tw1.ru']
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
     SECURE_SSL_REDIRECT = True
     SESSION_COOKIE_SECURE = True
